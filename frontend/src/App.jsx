71d8f0dfbdf854b59b3dcdb1b8b073320a311573
Gero <gtutos3@gmail.com>
2024-10-16 21:36:57 -0500

    Forntend - Se hizo pull de la rama julian

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,19 +13,23 @@ function App() {
   const [IsLogin, SetIsLogin] = useState(false);
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<NotFoundPage />} />
-          <Route path="/Inicio" element={<HomePage />} />
-          <Route path="/Perfil" element={<ProfilePage />} />
-          <Route path="/Mensajes" element />
-          <Route path="/Productos" element={<ArticlesPage/>}/>
-          <Route path="/Settings" element />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      {IsLogin ? (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+            <Route path="/Inicio" element={<HomePage />} />
+            <Route path="/Perfil" element={<ProfilePage />} />
+            <Route path="/Mensajes" element />
+            <Route path="/Productos" element={<ArticlesPage />} />
+            <Route path="/Settings" element />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      ) : (
+        <LoginPage />
+      )}
     </>
   );
 }
