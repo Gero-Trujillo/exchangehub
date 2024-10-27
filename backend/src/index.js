6d493fe9597a2ac2449476dccb5b23d3fd2c1862
Juanprm03 <juanpabloruizmarin333@gmail.com>
2024-10-27 14:59:40 -0500

    Backend - Implement socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,21 +2,28 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import userRouter from './Routes/userRoute.js'
-import chatRouter from './Routes/chatRoute.js'
-import messageRouter from './Routes/messageRoute.js'
-import ratingRouter from './Routes/ratingRoute.js'
+import userRouter from './routes/user.Route.js'
+import chatRouter from './routes/chat.Route.js'
+import messageRouter from './routes/message.Route.js'
+import ratingRouter from './routes/rating.Route.js'
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
-import express from "express";
+import {Server}  from 'socket.io';
+
 
 const app = express()
 dotenv.config()
 
 // Middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(authRoutes)
 app.use('/api/users', userRouter);
 app.use('/api/chats', chatRouter);
 app.use('/api/messages', messageRouter);
@@ -31,7 +38,7 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
 
-app.listen(port, (req, res) =>{
+const expressServer = app.listen(port, (req, res) =>{
     console.log(`Server running on port ${port}`)
 })
 
@@ -43,20 +50,48 @@ mongoose.connect(uri, {
     console.log('MongoDB connected')
 }).catch((err) => console.log(err))
 
-
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
-app.use(authRoutes)
-
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
 
+const io = new Server(expressServer, { cors: {origin: process.env.CLIENT_URL}})
+
+let onlineUsers = []
+
+io.on("connection", (socket) => {
+    console.log("new connection", socket.id)
+
+    // listen to a connection
+socket.on("addNewUser", (userId) => {
+    !onlineUsers.some((user) => user.userId === userId) &&
+        onlineUsers.push({
+            userId,
+            socketId: socket.id
+        });
+
+    console.log("onlineUsers", onlineUsers); 
+    
+    io.emit("getOnlineUsers", onlineUsers);
+        
+    });
+
+    // add message
+    socket.on("sendMessage", (message) => {
+        const user = onlineUsers.find(user => user.userId === message.recipientId);
+
+        if (user){
+            io.to(user.socketId).emit("getMessage", message);
+            io.to(user.socketId).emit("getNotification", {
+                senderId: message.senderId,
+                isRead: false,
+                date: new Date()
+            });
+        }
+    })
+
+    socket.on("disconnect", () => {
+        onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id);
+
+        io.emit("getOnlineUsers", onlineUsers);
+    })
+})
