ddf4b52acb95e52284d129b55f9d0f8af1a611ad
gero-trujillo <gtutos3@gmail.com>
2024-10-28 14:49:59 -0500

    Frontend - Llamado de productos en el home, seccion de mas populares

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,39 @@
 import AdvantagesHome from "../components/AdvantagesHome";
 import ProductCard from "../components/ProductCard";
 import Button1 from "../components/Button1";
+import { useEffect, useState } from "react";
+import { useArticle } from "../context/ArticleContext.jsx";
 function HomePage() {
+  const { getAllArticles, articles, getArticlesImages, articleImgs } =
+    useArticle();
+
+  const [articlesImages, setArticlesImages] = useState([]);
+
+  useEffect(() => {
+    getAllArticles();
+  }, []);
+
+  useEffect(() => {
+    // Función para obtener las imágenes de los artículos
+    const fetchArticlesImages = async () => {
+      try {
+        const images = {};
+        for (const article of articles) {
+          const articleImages = await getArticlesImages(article.idArticle);
+          images[article.idArticle] = articleImages;
+        }
+
+        setArticlesImages(images);
+      } catch (error) {
+        console.error("Error fetching article images:", error);
+      }
+    };
+
+    if (articles.length > 0) {
+      fetchArticlesImages();
+    }
+  }, [articles]);
+
   return (
     <>
       <section className="flex justify-center items-center min-h-full p-20 w-full max-w-full">
@@ -19,18 +51,22 @@ function HomePage() {
         </div>
       </section>
       <section className="bg-neutral-100 dark:bg-zinc-900 m-10 rounded-lg p-10 h-auto flex flex-col justify-center items-center gap-10">
-        <h1 className="text-4xl font-bold text-emerald-600 dark:text-emerald-300">Mas populares</h1>
+        <h1 className="text-4xl font-bold text-emerald-600 dark:text-emerald-300">
+          Mas populares
+        </h1>
         <div className="flex flex-wrap gap-8 w-full justify-center">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {articles.map((article) => {
+            return (
+              <ProductCard
+                key={article.idArticle}
+                idArticle={article.idArticle}
+                name={article.name}
+                user={article.idOwner}
+                description={article.description}
+                images={articlesImages[article.idArticle]}
+              />
+            );
+          })}
         </div>
       </section>
       <AdvantagesHome />
