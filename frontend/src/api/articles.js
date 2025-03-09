840c9b77013b94c831347f3ccc53adcea5b3613d
Gero <gtutos3@gmail.com>
2025-03-09 12:55:48 -0500

    Frontend - Edit article and images has been complete

--- frontend/src/api/articles.js
+++ frontend/src/api/articles.js
@@ -3,6 +3,7 @@ import axios from "./axios.js";
 export const getArticles = () => axios.get("/articles");
 export const getArticlesByUserId = (id) => axios.get(`/articles/user/${id}`);
 export const createArticleImage = (data) => axios.post("/articles/image", data);
+export const deleteArticleImage = (id) => axios.delete(`/articles/image/${id}`);
 export const createArticle = (data) => axios.post("/articles", data);
 export const getArticleImages = (id) => axios.get(`/articles/image/${id}`);
 export const deleteArticle = (id) => axios.delete(`/articles/${id}`)
@@ -10,8 +11,5 @@ export const getArticlesBySearch = (search) =>
   axios.get(`/articles/search/${search}`);
 export const getArticlesByCategory = (category) =>
   axios.get(`/articles/category/${category}`);
-export const getArticleById = async (articleId) => {
-  const response = await axios.get(`/articles/${articleId}`);
-  return response.data;
-};
+export const getArticleById = (id) => axios.get(`/articles/${id}`);
 export const updateArticle = (articleId, data) => axios.put(`/articles/${articleId}`, data);
